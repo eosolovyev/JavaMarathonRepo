4b208a369ff98394537f6352df5dfb218ebf724d
esolovyev <eugeny.solovyov@mera.com>
2020-08-24 12:57:58 +0300

    Add day1 solution

---
+++ src/day1/Task1.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            a++;
+            System.out.print("JAVA ");
+        }
+    }
+}
---
+++ src/day1/Task4.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int age = 31;
+        while (age < 96) {
+            age = age + 5;
+            System.out.println("Мой возраст " + age);
+        }
+    }
+}
